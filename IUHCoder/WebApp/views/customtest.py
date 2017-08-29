d7270a62532681da1b0bee48d0cfd63e1a7defe4
Quốc Việt <vuongquocviet1996@gmail.com>
2017-08-30 00:45:26 +0700

    add bootstrap library  + create a few new page

--- IUHCoder/WebApp/views/customtest.py
+++ IUHCoder/WebApp/views/customtest.py
@@ -1,11 +1,14 @@
 from flask import Blueprint, render_template, request
+from Compiler.Languages.writeFile import writeFile as wf
 
-bp = Blueprint(__name__, __name__, template_folder = 'templates')
+bp = Blueprint(__name__, __name__, template_folder='templates')
+
 
 @bp.route('/customtest', methods=['POST', 'GET'])
 def show():
-	if request.method == 'POST':
-		if request.form.get('customtest'):
-			text = request.form.get('notetext')
-			return text
-	return render_template('customtest.html')
+    if request.method == 'POST':
+        if request.form.get('customtest'):
+            text = request.form.get('codetext')
+            wf(text)
+
+    return render_template("customtest.html")
