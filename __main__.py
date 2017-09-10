7d534b979cd4cf163e2cff0eb282454022cbffbd
Quốc Việt <vuongquocviet1996@gmail.com>
2017-09-10 11:03:54 +0700

    Update __main__.py port 80

--- __main__.py
+++ __main__.py
@@ -1,4 +1,4 @@
 from WebApp.app import app
 
 if __name__ == '__main__':
-    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
+    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 80)))
